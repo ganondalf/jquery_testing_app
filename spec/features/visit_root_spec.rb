8be1139a6fd98bc69567d54931cc30d573c9404b
ganondalf <cganon@gmail.com>
2014-04-25 15:30:08 -0400

    make text pass

--- spec/features/visit_root_spec.rb
+++ spec/features/visit_root_spec.rb
@@ -2,14 +2,9 @@ require 'spec_helper'
 
 describe "root page" do
   describe "clicking on the header", js: true do
-      it "text should display" do
+      it "text should display the text in the header backwards" do
         visit '/'
         expect(page).to have_content("Hey Macarena")
       end
-      it "text should display" do
-        visit '/'
-        find("h1").click
-      end
-
    end
 end
